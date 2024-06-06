39c43e5f263d50a4faab1b22b5f5a3da438d7fd2
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-06-07 01:22:06 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Menu from './Menu'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
+import Link from 'next/link'
 export default function Header() {
   return (
     <div className='flex justify-between p-3 max-w-6xl mx-auto' >
@@ -9,8 +10,12 @@ export default function Header() {
         <Menu title='home' address='/' Icon={AiFillHome} />
         <Menu title='about' address='/about' Icon={BsFillInfoCircleFill} />
       </div>
+      
       <div className="flex gap-1 items-center">
-        <span className='text-2xl font-serif font-bold mr-2'>Cinema</span><span className='text-lg font-bold bg-orange-500 p-1 rounded-lg' >Info</span>
+      <Link>
+         <span className='text-2xl font-serif font-bold mr-2'>Cinema</span><span className='text-lg font-bold bg-orange-500 p-1 rounded-lg' >Info</span>
+      </Link>
+        
       </div>
     </div>
   )
