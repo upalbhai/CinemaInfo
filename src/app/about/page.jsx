705696ca0fac13d299ee383ebc8889d6d84597f3
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-06-07 17:47:36 +0530

    created about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='text-justify max-w-6xl mx-auto p-3 space-y-4' >
+      <h1 className='text-2xl text-bold text-slate-950 dark:text-orange-500' >About</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!
+      </p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto voluptatem voluptas, nam distinctio iure optio nesciunt labore nisi quia facilis dolorem autem repudiandae necessitatibus explicabo omnis a odit dolorum!</p>
+    </div>
+  )
+}
